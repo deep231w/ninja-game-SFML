5e2c0bc3c01697d9cc8805e8c9318d16552f2906
Deepak Maharana <100968930+deep231w@users.noreply.github.com>
2026-04-23 21:54:40 +0530

    escape button to close game added

--- main.cpp
+++ main.cpp
@@ -43,6 +43,9 @@ int main() {
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
+
+            if(event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard ::Escape)
+                window.close();
         }
 
         //movement ninja character
